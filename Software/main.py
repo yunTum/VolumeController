89fe8631cdbda8b40fc3fa27269e3fc330a36c0f
yunTum <yusukeaonuma616@gmail.com>
2023-07-11 22:03:44 +0900

    ボリューム機能の追加

--- Software/main.py
+++ Software/main.py
@@ -6,7 +6,8 @@ import time
 from pycaw.pycaw import AudioUtilities
 import pyautogui
 import serial
-
+from PIL import Image
+from pystray import Icon, Menu, MenuItem
 class AudioController:
     def __init__(self, process_name):
         self.process_name = process_name
@@ -76,27 +77,49 @@ def main():
         ser = serial.Serial('COM3', 115200, timeout=0.1)
     except:
         print("Serial not found")
-
+    
+    # def quit_app():
+    #     icon.stop()
+    # def run_app():
+    #     pass
+    # image = Image.open('e.ico')
+    # menu = Menu(MenuItem('Quit', quit_app), MenuItem('Run', run_app))
+    # icon = Icon(name='test', icon=image, title='pystray App', menu=menu)
+    # icon.run()
     audio_controller = AudioController("chrome.exe")
     action = ""
     vlm_val = 0
+    pre_vlm_val = 0
+    delta_vlm = 1/1023
+    
     try:
+        val_arduino = ser.readline()
+        val_arduino = val_arduino.decode('utf-8')
+        split_val = val_arduino.split(',')
+        if(len(split_val) > 1): 
+            # ボリュームの初期値を取得
+            vlm_val = int(split_val[1].replace('\r\n', ''))
+        # 接続待ちにするために2.5秒待機
+        print("接続待ちにするために2.5秒待機")
+        time.sleep(2.5)
+        print("接続待ち終了")
         while(True):
             val_arduino = ser.readline()
             val_arduino = val_arduino.decode('utf-8')
             split_val = val_arduino.split(',')
-            if(len(split_val) > 1): 
+            if(len(split_val) > 1):
                 action = split_val[0]
-                
-                # ボリューム値を取得、使用先は未実装
+                pre_vlm_val = vlm_val
+                # ボリューム値を取得
                 vlm_val = int(split_val[1].replace('\r\n', ''))
+                
             if action == '#tct2':
                 print("tct2ボタンが押されました")
-                audio_controller.increase_volume(0.05)
+                audio_controller.increase_volume(0.01)
                 
             if action == '#tct1':
                 print("tct1ボタンが押されました")
-                audio_controller.decrease_volume(0.05)
+                audio_controller.decrease_volume(0.01)
             
             if action == '#tct3':
                 print("tct3ボタンが押されました")
@@ -111,6 +134,9 @@ def main():
             if action == '#tct6':
                 print("tct6ボタンが押されました")
             
+            # vlm_valの変更があった場合、処理を実行
+            if vlm_val != pre_vlm_val:
+                audio_controller.set_volume(vlm_val * delta_vlm)
             # 待機時間: 0.05秒
             time.sleep(0.05)
             
