0719dc19afc25d2ec58a88963a84818e14bb73ec
yunTum <yusukeaonuma616@gmail.com>
2023-09-17 16:38:56 +0900

    クラス化と常駐化の実装

--- Software/main.py
+++ Software/main.py
@@ -3,11 +3,14 @@ SimpleVolumeController.
 """
 #!/usr/bin/env python
 import time
-from pycaw.pycaw import AudioUtilities
 import pyautogui
 import serial
+import math
+import threading
+from pycaw.pycaw import AudioUtilities
+from pystray import Icon,Menu,MenuItem
 from PIL import Image
-from pystray import Icon, Menu, MenuItem
+
 class AudioController:
     def __init__(self, process_name):
         self.process_name = process_name
@@ -67,84 +70,117 @@ class AudioController:
                 interface.SetMasterVolume(self.volume, None)
                 print("Volume raised to", self.volume)  # debug
 
-def main():
-    try:
-        '''
-            Arduinoのシリアルポートを指定
-            ボーレート: 115200
-            COMポート: COM3
-        '''
-        ser = serial.Serial('COM3', 115200, timeout=0.1)
-    except:
-        print("Serial not found")
+class SystemTrayIcon:
+    def __init__(self):
+        self.icon = None
+        self.thread = threading.Thread(target=self.tasktray_create)
+        self.thread.start()
+        self.com_hard = CommicationHard()
+        self.com_hard.connect_serial()
     
-    # def quit_app():
-    #     icon.stop()
-    # def run_app():
-    #     pass
-    # image = Image.open('e.ico')
-    # menu = Menu(MenuItem('Quit', quit_app), MenuItem('Run', run_app))
-    # icon = Icon(name='test', icon=image, title='pystray App', menu=menu)
-    # icon.run()
-    audio_controller = AudioController("chrome.exe")
-    action = ""
-    vlm_val = 0
-    pre_vlm_val = 0
-    delta_vlm = 1/1023
+    def tasktray_create(self):
+        try:
+            item=[]
+            options_map = {'Test': lambda: self.tasktray_test(),'Quit': lambda: self.tasktray_abort()}
     
-    try:
-        val_arduino = ser.readline()
+            for option,callback in options_map.items():
+                item.append( MenuItem(option,callback,default=True if option == 'Show' else False ) )
+                
+            menu = Menu(*item)
+
+            image = Image.open("app.ico")
+            self.icon = Icon("name",image,"My System Tray Icon", menu)
+            self.icon.run()
+        finally:
+            self.tasktray_abort()
+
+    def tasktray_test(self):
+        print("TaskTray Test")
+        
+    def tasktray_abort(self):
+        if self.icon != 0:
+            self.icon.stop()
+            self.com_hard.set_end_status(False)
+
+class CommicationHard:
+    def __init__(self):
+        self.ser = None
+        self.audio_controller = AudioController("chrome.exe")
+        self.action = ""
+        self.vlm_val = 0
+        self.pre_vlm_val = 0
+        self.delta_vlm = 1/1023
+        self.end_status = True
+        self.theard = threading.Thread(target=self.run)
+
+    def connect_serial(self):
+        try:
+            '''
+                Arduinoのシリアルポートを指定
+                ボーレート: 115200
+                COMポート: COM3
+            '''
+            self.ser = serial.Serial('COM3', 115200, timeout=0.1)
+            try:
+                self.get_signal()
+                # 接続待ちにするために2.5秒待機
+                print("接続待ちにするために2.5秒待機")
+                time.sleep(2.5)
+                print("接続待ち終了")
+                self.theard.start()
+                
+            except:
+                print("Serial can not read")
+        except:
+            print("Serial not found")
+
+    def get_signal(self):
+        val_arduino = self.ser.readline()
         val_arduino = val_arduino.decode('utf-8')
         split_val = val_arduino.split(',')
-        if(len(split_val) > 1): 
+        if(len(split_val) > 1):
             # ボリュームの初期値を取得
-            vlm_val = int(split_val[1].replace('\r\n', ''))
-        # 接続待ちにするために2.5秒待機
-        print("接続待ちにするために2.5秒待機")
-        time.sleep(2.5)
-        print("接続待ち終了")
-        while(True):
-            val_arduino = ser.readline()
-            val_arduino = val_arduino.decode('utf-8')
-            split_val = val_arduino.split(',')
-            if(len(split_val) > 1):
-                action = split_val[0]
-                pre_vlm_val = vlm_val
-                # ボリューム値を取得
-                vlm_val = int(split_val[1].replace('\r\n', ''))
-                
-            if action == '#tct2':
-                print("tct2ボタンが押されました")
-                audio_controller.increase_volume(0.01)
-                
-            if action == '#tct1':
-                print("tct1ボタンが押されました")
-                audio_controller.decrease_volume(0.01)
+            self.action = split_val[0]
+            self.pre_vlm_val = self.vlm_val
+            # ボリューム値を取得
+            self.vlm_val = int(split_val[1].replace('\r\n', ''))
+            self.vlm_val = math.floor(self.vlm_val * self.delta_vlm * 100) / 100
             
-            if action == '#tct3':
-                print("tct3ボタンが押されました")
-                pyautogui.keyDown('playpause')
-            
-            if action == '#tct4':
-                print("tct4ボタンが押されました")
-                
-            if action == '#tct5':
-                print("tct5ボタンが押されました")
-                
-            if action == '#tct6':
-                print("tct6ボタンが押されました")
-            
-            # vlm_valの変更があった場合、処理を実行
-            if vlm_val != pre_vlm_val:
-                audio_controller.set_volume(vlm_val * delta_vlm)
-            # 待機時間: 0.05秒
-            time.sleep(0.05)
-            
-    except KeyboardInterrupt:
-        print("例外'KeyboardInterrupt'")
-        ser.close()
+    def run(self):
+        while(self.end_status):
+            try:
+                self.get_signal()
+                if self.action == '#tct1':
+                    print("tct1ボタンが押されました")
+                    pyautogui.hotkey("ctrl", "shift", "l")
+                if self.action == '#tct2':
+                    print("tct2ボタンが押されました")
+                    pyautogui.hotkey("win", "tab")
+                if self.action == '#tct3':
+                    print("tct3ボタンが押されました")
+                    pyautogui.keyDown('playpause')
+                if self.action == '#tct4':
+                    print("tct4ボタンが押されました")
+                if self.action == '#tct5':
+                    print("tct5ボタンが押されました")     
+                if self.action == '#tct6':
+                    print("tct6ボタンが押されました")
+                # vlm_valの変更があった場合、処理を実行
+                if self.vlm_val != self.pre_vlm_val:
+                    self.audio_controller.set_volume(self.vlm_val)
+                # 待機時間: 0.05秒
+                time.sleep(0.05)
+            except KeyboardInterrupt:
+                print("例外'KeyboardInterrupt'")
+                self.ser.close()
+                self.theard.join()
     print("処理を終了します")
+    
+    def set_end_status(self, status):
+        self.end_status = status
 
+def main():
+    tray = SystemTrayIcon()
 
 if __name__ == "__main__":
     main()
